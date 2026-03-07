2d5279c82be7d1aac4fd0714d56edd4729116a57
popoolarofih <popoolarofih@gmail.com>
2026-03-07 14:34:59 +0100

    modified:   components/admin/AdminLayout.js

--- components/admin/AdminLayout.js
+++ components/admin/AdminLayout.js
@@ -47,7 +47,7 @@ export default function AdminLayout({ children, title }) {
         {/* Logo */}
         <div className="flex items-center gap-3 px-6 py-6 border-b border-white/10">
           <div className="w-9 h-9 bg-white/15 rounded-xl flex items-center justify-center border border-white/20">
-            <HeartHandshake className="w-5 h-5 text-primary-300" />
+            <img src="https://apffhecod.netlify.app/assets/img/LOGO.jpg" alt="APFFHECOD Logo" className=" object-contain" />
           </div>
           <div>
             <p className="font-display font-bold text-white text-sm">APFFHECOD</p>
